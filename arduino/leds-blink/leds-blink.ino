b7b6c477afc1d133e33315ba84411325d6a7b44e
gathuaalex <alexgathua3@gmail.com>
2021-08-31 16:48:05 +0300

    commented on the led blinking code

--- arduino/leds-blink/leds-blink.ino
+++ arduino/leds-blink/leds-blink.ino
@@ -1,27 +1,28 @@
 // declare pins to blink leds
-int led1=13,led2=12,led3=11,led4=10,led5=9;
+int led1=13;
 
 //array of leds
-int leds[5]={led1,led2,led3,led4,led5};
+int leds[5]={led1};
 
 void setup() {
 // setting all declared pins as outputs
     pinMode(led1,OUTPUT);
-    pinMode(led2,OUTPUT);
-    pinMode(led3,OUTPUT);
-    pinMode(led4,OUTPUT);
-    pinMode(led5,OUTPUT);
 }
 
 void loop() {
 //function to blink leds
-  blink_leds();
+  //blink_leds();
+   digitalWrite(led1,HIGH);
+    delay(200);
+    digitalWrite(led1,LOW);
+    delay(200); 
 }
-void blink_leds(){
-// loop over the pins in the array and perform writing the high and low
-  for (int i=0;i<sizeof(leds);i++){
-    digitalWrite(leds[i],HIGH);
-    delay(70);
-    digitalWrite(leds[i],LOW);
-    }
-  }
+//void blink_leds(){
+//// loop over the pins in the array and perform writing the high and low
+//  for (int i=0;i<sizeof(leds);i++){
+//    digitalWrite(leds[i],HIGH);
+//    delay(70);
+//    digitalWrite(leds[i],LOW);
+//    
+//    }
+//  }
